b2857a57d2cc9d3283dd3118ce755027296df0b0
ArturKhudaverdiev <arturuskh@gmail.com>
2023-07-08 22:52:50 +0300

    new testing for Apple website

---
+++ BrowserStack/helpers.py
@@ -0,0 +1,349 @@
+# HOME PAGE
+
+# url Apple website
+url = "https://www.apple.com/"
+
+#Main apple icon
+apple_icon = "(//a[contains(.,'Apple')])[1]"
+
+#top menu
+
+store = "//span[@class='globalnav-link-text-container'][contains(.,'Store')]"
+mac = "//span[@class='globalnav-link-text-container'][contains(.,'Mac')]"
+ipad = "//span[@class='globalnav-link-text-container'][contains(.,'iPad')]"
+iphone = "//span[@class='globalnav-link-text-container'][contains(.,'iPhone')]"
+watch = "//span[@class='globalnav-link-text-container'][contains(.,'Watch')]"
+vision = "//span[@class='globalnav-link-text-container'][contains(.,'Vision')]"
+airpods = "//span[@class='globalnav-link-text-container'][contains(.,'Vision')]"
+tv_home = "//span[@class='globalnav-link-text-container'][contains(.,'Vision')]"
+entertainment = "//span[@class='globalnav-link-text-container'][contains(.,'Entertainment')]"
+accessories = "//span[@class='globalnav-link-text-container'][contains(.,'Accessories')]"
+support = "//span[@class='globalnav-link-text-container'][contains(.,'Support')]"
+search = "//a[@id='globalnav-menubutton-link-search']"
+search_input_field = "//input[contains(@class,'globalnav-searchfield-input')]"
+bag = "//a[@id='globalnav-menubutton-link-bag']"
+
+#Image Carousel on home page
+carousel1 = "//a[@id='tv-plus-gallery-item-1-trigger']"
+carousel2 = "//a[@id='tv-plus-gallery-item-2-trigger']"
+carousel3 = "//a[@id='tv-plus-gallery-item-3-trigger']"
+carousel4 = "//a[@id='tv-plus-gallery-item-4-trigger']"
+carousel5 = "//a[@id='tv-plus-gallery-item-5-trigger']"
+carousel6 = "//a[@id='tv-plus-gallery-item-6-trigger']"
+carousel7 = "//a[@id='tv-plus-gallery-item-7-trigger']"
+carousel8 = "//a[@id='tv-plus-gallery-item-8-trigger']"
+carousel9 = "//a[@id='tv-plus-gallery-item-9-trigger']"
+carousel10 = "//a[@id='tv-plus-gallery-item-10-trigger']"
+carousel11 = "//a[@id='tv-plus-gallery-item-11-trigger']"
+carousel12 = "//a[@id='tv-plus-gallery-item-12-trigger']"
+
+#FOOTER MENU
+#Shop and Learn
+f_store = "(//a[contains(.,'Store')])[5]"
+f_mac = "(//a[contains(.,'Mac')])[18]"
+f_ipad = "(//a[contains(.,'iPad')])[18]"
+f_iphone = "(//a[contains(.,'iPhone')])[16]"
+f_i_watch = "(//a[contains(.,'Watch')])[18]"
+f_vision = "(//a[contains(.,'Vision')])[3]"
+f_air_pods = "(//a[contains(.,'AirPods')])[14]"
+f_tv_home = "(//a[contains(.,'TV & Home')])[5]"
+f_air_tag = "(//a[contains(.,'AirTag')])[3]"
+f_accessories = "(//a[contains(.,'Accessories')])[10]"
+f_gift_cards = "//a[contains(.,'Gift Cards')]"
+
+#Apple Wallet
+f_wallet = "(//a[contains(.,'Wallet')])[2]"
+f_apple_card = "(//a[@href='/apple-card/'])[3]"
+f_apple_pay = "//a[@href='/apple-pay/']"
+f_apple_cash = "//a[@href='/apple-cash/']"
+
+#Account
+f_manage_your_apple_id = "(//a[@class='ac-gf-directory-column-section-link'])[16]"
+f_apple_store_account = "//a[@href='/us/shop/goto/account']"
+f_i_cloud_id = "//a[contains(.,'iCloud.com')]"
+
+#Entertainment
+f_apple_one = "(//a[@href='/apple-one/'])[2]"
+f_apple_tv = "(//a[@href='/apple-tv-plus/'])[3]"
+f_apple_music = "(//a[@href='/apple-music/'])[4]"
+f_apple_arcade = "(//a[@href='/apple-arcade/'])[2]"
+f_apple_fitness = "(//a[@href='/apple-fitness-plus/'])[3]"
+f_apple_news = "(//a[@href='/apple-news/'])[2]"
+f_apple_podcasts = "(//a[@href='/apple-podcasts/'])[2]"
+f_apple_books = "(//a[@href='/apple-books/'])[2]"
+f_app_store = "(//a[@href='/app-store/'])[2]"
+
+#Apple Store
+f_find_a_store = "(//a[@href='/retail/'])[2]"
+f_genius_bar = "//a[@href='/retail/geniusbar/']"
+f_today_at_apple = "//a[@href='/today/']"
+f_apple_camp = "//a[@href='/today/camp/']"
+f_apple_store_app = "//a[contains(.,'Apple Store App')]"
+f_certified_refurbished = "(//a[contains(.,'Certified Refurbished')])[2]"
+f_apple_trade_in = "(//a[contains(@data-analytics-title,'apple trade in')])[6]"
+f_financing = "(//a[contains(.,'Financing')])[6]"
+f_carrier_deals_at_apple = "(//a[contains(.,'Carrier Deals at Apple')])[2]"
+f_order_status = "(//a[contains(.,'Order Status')])[2]"
+shopping_help = "//a[contains(.,'Shopping Help')]"
+
+#For Business
+f_apple_and_business = "//a[contains(.,'Apple and Business')]"
+f_shop_for_business = "//a[contains(.,'Shop for Business')]"
+
+#For Education
+f_apple_and_education = "//a[contains(.,'Apple and Education')]"
+f_shop_for_k12 = "//a[contains(.,'Shop for K-12')]"
+f_shop_for_college = "//a[contains(.,'Shop for College')]"
+
+#For Healthcare
+f_apple_in_healthcare = "//a[contains(.,'Apple in Healthcare')]"
+f_health_on_apple_watch = "//a[contains(.,'Health on Apple Watch')]"
+f_health_records_on_iphone = "//a[contains(.,'Health Records on iPhone')]"
+
+#For Government
+f_shop_for_government = "//a[contains(.,'Shop for Government')]"
+f_shop_for_veterans_and_military = "//a[contains(.,'Shop for Veterans and Military')]"
+
+#Apple Values
+f_accessibility = "(//a[contains(.,'Accessibility')])[2]"
+f_education = "(//a[contains(.,'Education')])[5]"
+f_environment = "//a[contains(.,'Environment')]"
+f_inclusion_and_diversity = "//a[contains(.,'Inclusion and Diversity')]"
+f_privacy = "(//a[contains(.,'Privacy')])[2]"
+f_racial_equity_and_justice = "//a[contains(.,'Racial Equity and Justice')]"
+f_supplier_responsibility = "//a[contains(.,'Supplier Responsibility')]"
+
+#About apple
+f_newsroom = "//a[contains(.,'Newsroom')]"
+f_apple_leadership = "//a[contains(.,'Apple Leadership')]"
+f_career_opportunities = "//a[contains(.,'Career Opportunities')]"
+f_investors = "//a[contains(.,'Investors')]"
+f_ethics_and_compliance = "//a[contains(.,'Ethics & Compliance')]"
+f_events = "//a[contains(.,'Events')]"
+f_contact_apple = "//a[contains(.,'Contact Apple')]"
+
+#Copyright © 2023 Apple Inc. All rights reserved.
+f_privacy_policy = "//a[contains(.,'Privacy Policy')]"
+f_terms_of_use = "//a[contains(.,'Terms of Use')]"
+f_sales_and_refunds = "//a[contains(.,'Sales and Refunds')]"
+f_legal = "//a[contains(.,'Legal')]"
+f_site_map = "//a[contains(.,'Contact Apple')]"
+
+#Footer choose country
+f_country = "//a[@href='/choose-country-region/']"
+
+#DROP DOWN TOP MENU
+
+#STORE
+#Shop
+s_shop_the_latest = "//a[contains(.,'Shop the Latest')]"
+s_mac = "(//a[contains(.,'Mac')])[1]"
+s_ipad = "(//a[contains(.,'iPad')])[1]"
+s_iphone = "(//a[contains(.,'iPhone')])[1]"
+s_apple_watch = "(//a[contains(.,'Apple Watch')])[1]"
+s_accessories = "(//a[contains(.,'Accessories')])[1]"
+
+#Quick links
+s_find_a_store = "(//a[contains(.,'Find a Store')])[1]"
+s_order_status = "(//a[contains(.,'Order Status')])[1]"
+s_apple_trade_in = "(//a[contains(.,'Apple Trade In')])[1]"
+s_financing = "(//a[contains(.,'Financing')])[1]"
+s_college_student_offer = "(//a[contains(.,'College Student Offer')])[1]"
+
+#Shop Special Stores
+s_certified_refurbished = "(//a[contains(.,'Certified Refurbished')])[1]"
+s_education = "(//a[contains(.,'Education')])[1]"
+s_business = "(//a[contains(.,'Business')])[1]"
+s_veterans_and_military = "(//a[contains(.,'Veterans and Military')])[1]"
+s_government = "(//a[contains(.,'Government')])[1]"
+
+#MAC
+#Explore Mac
+
+s_explore_all_mac = "//a[contains(.,'Explore All Mac')]"
+s_macbook_air = "//a[contains(.,'MacBook Air')]"
+s_macbook_pro = "//a[contains(.,'MacBook Pro')]"
+s_i_mac = "//a[contains(.,'iMac')]"
+s_mac_mini = "//a[contains(.,'Mac mini')]"
+s_mac_studio = "//a[contains(.,'Mac Studio')]"
+s_mac_pro = "//a[contains(.,'Mac Pro')]"
+s_displays = "//a[contains(.,'Displays')]"
+s_compare_mac = "//a[contains(.,'Compare Mac')]"
+s_mac_does_that = "//a[contains(.,'Mac Does That')]"
+
+#Shop Mac
+s_shop_mac = "//a[contains(.,'Shop Mac')]"
+s_mac_accessories = "//a[contains(.,'Mac Accessories')]"
+s_apple_trade_in_mac = "(//a[contains(.,'Apple Trade In')])[2]"
+s_financing_mac = "(//a[contains(.,'Financing')])[2]"
+s_college_student_offer_mac = "(//a[contains(.,'College Student Offer')])[2]"
+
+#More from Mac
+s_mac_support = "//a[contains(.,'Mac Support')]"
+s_macos_sonoma_preview = "//a[contains(.,'macOS Sonoma Preview')]"
+s_final_cut_pro = "(//a[contains(.,'Final Cut Pro')])[1]"
+s_logic_pro = "(//a[contains(.,'Logic Pro')])[1]"
+s_continuity = "//a[contains(.,'Continuity')]"
+s_i_cloud = "(//a[contains(.,'iCloud')])[1]"
+s_mac_for_business = "//a[contains(.,'Mac for Business')]"
+s_education_mac = "(//a[contains(.,'Education')])[2]"
+
+#IPAD
+#Explore iPad
+s_explore_all_ipad = "//a[contains(.,'Explore All iPad')]"
+s_ipad_pro = "//a[contains(.,'iPad Pro')]"
+s_ipad_air = "//a[contains(.,'iPad Air')]"
+s_ipad_ipad = "(//a[contains(.,'iPad')])[6]"
+s_ipad_mini = "//a[contains(.,'iPad mini')]"
+s_apple_pencil = "//a[contains(.,'Apple Pencil')]"
+s_keyboards = "//a[contains(.,'Keyboards')]"
+s_compare_ipad = "//a[contains(.,'Compare iPad')]"
+s_why_ipad = "//a[contains(.,'Why iPad')]"
+
+#Shop iPad
+s_shop_ipad = "//a[contains(.,'Shop iPad')]"
+s_ipad_accessories = "//a[contains(.,'iPad Accessories')]"
+s_apple_trade_in_ipad = "(//a[contains(.,'Apple Trade In')])[3]"
+s_financing_ipad = "(//a[contains(.,'Financing')])[3]"
+s_college_student_offer_ipad = "(//a[contains(.,'College Student Offer')])[3]"
+
+#More from iPad
+s_ipad_support = "//a[contains(.,'iPad Support')]"
+s_final_cut_pro_for_ipad = "//a[contains(.,'Final Cut Pro for iPad')]"
+s_logic_pro_for_ipad = "//a[contains(.,'Logic Pro for iPad')]"
+s_icloud_ipad = "(//a[contains(.,'iCloud')])[2]"
+s_education_ipad = "(//a[contains(.,'Education')])[3]"
+
+#IPHONE
+#Explore iPhone
+s_explore_all_iphone = "//a[contains(.,'Explore All iPhone')]"
+s_compare_iphone = "//a[contains(.,'Compare iPhone')]"
+s_switch_from_android = "//a[contains(.,'Switch from Android')]"
+
+#Shop iPhone
+s_shop_iphone = "//a[contains(.,'Shop iPhone')]"
+s_iphone_accessories = "//a[contains(.,'iPhone Accessories')]"
+s_apple_trade_in_iphone = "(//a[contains(.,'Apple Trade In')])[4]"
+s_carrier_deals_at_apple = "(//a[contains(.,'Carrier Deals at Apple')])[1]"
+s_financing_iphone = "(//a[contains(.,'Financing')])[4]"
+
+#More from Iphone
+s_iphone_support = "//a[contains(.,'iPhone Support')]"
+s_iphone_privacy = "//a[contains(.,'iPhone Privacy')]"
+s_icloud_iphone = "(//a[contains(.,'iCloud')])[3]"
+s_wallet_pay_card = "//a[contains(.,'Wallet, Pay, Card')]"
+s_siri = "(//a[contains(.,'Siri')])[1]"
+
+#WATCH
+#Explore watch
+s_explore_all_apple_watch = "//a[contains(.,'Explore All Apple Watch')]"
+s_compare_watch = "//a[contains(.,'Compare Watch')]"
+s_why_apple_watch = "//a[contains(.,'Why Apple Watch')]"
+
+#Shop watch
+s_shop_apple_watch = "//a[contains(.,'Shop Apple Watch')]"
+s_apple_watch_studio = "//a[contains(.,'Apple Watch Studio')]"
+s_apple_watch_bands = "//a[contains(.,'Apple Watch Bands')]"
+s_apple_watch_accessories = "//a[contains(.,'Apple Watch Accessories')]"
+s_apple_trade_in_watch = "(//a[contains(.,'Apple Trade In')])[5]"
+s_financing_watch = "(//a[contains(.,'Financing')])[5]"
+
+#More from Watch
+s_apple_watch_support = "//a[contains(.,'Apple Watch Support')]"
+s_apple_fitness = "(//a[contains(.,'Apple Fitness+')])[1]"
+
+#AIRPODS
+#ExploreAirPods
+s_explore_all_airpods = "//a[contains(.,'Explore All AirPods')]"
+s_compare_airpods = "//a[contains(.,'Compare AirPods')]"
+
+#ShopAirPods
+s_shop_airpods = "//a[contains(.,'Shop AirPods')]"
+s_airpods_accessories = "//a[contains(.,'AirPods Accessories')]"
+
+#More from AirPods
+s_airpods_support = "//a[contains(.,'AirPods Support')]"
+s_apple_music = "(//a[contains(.,'Apple Music')])[1]"
+
+# TV AND HOME
+#Explore TV & Home
+s_explore_tv_home = "//a[contains(.,'Explore TV & Home')]"
+s_apple_tv_4k = "(//a[contains(.,'Apple TV 4K')])[1]"
+s_homepod = "(//a[contains(.,'HomePod')])[1]"
+s_homepod_mini = "(//a[contains(.,'HomePod mini')])[1]"
+
+#Shop TV & Home
+s_shop_apple_tv_4k = "//a[contains(.,'Shop Apple TV 4K')]"
+s_shop_homepod = "(//a[contains(.,'Shop HomePod')])[1]"
+s_shop_homepod_mini = "//a[contains(.,'Shop HomePod mini')]"
+s_shop_siri_remote = "//a[contains(.,'Shop Siri Remote')]"
+s_tv_home_accessories = "//a[contains(.,'TV & Home Accessories')]"
+
+#More from TV & Home
+s_apple_tv_support = "//a[contains(.,'Apple TV Support')]"
+s_homepod_support = "//a[contains(.,'HomePod Support')]"
+s_apple_tv_app = "//a[contains(.,'Apple TV app')]"
+s_apple_tv_plus = "(//a[contains(.,'Apple TV+')])[1]"
+s_home_app = "//a[contains(.,'Home app')]"
+s_apple_music_tv_home = "(//a[contains(.,'Apple Music')])[2]"
+s_siri_tv_home = "(//a[contains(.,'Siri')])[3]"
+s_airplay = "//a[contains(.,'AirPlay')]"
+
+#ENTERTAINMENT
+#Explore entertainment
+s_explore_entertainment = "//a[contains(.,'Explore Entertainment')]"
+s_apple_one = "(//a[contains(.,'Apple One')])[1]"
+s_apple_tv_plus_entertainment = "(//a[contains(.,'Apple TV+')])[2]"
+s_apple_music_entertainment = "(//a[contains(.,'Apple Music')])[3]"
+s_apple_arcade = "(//a[contains(.,'Apple Arcade')])[1]"
+s_apple_fitness_entertainment = "(//a[contains(.,'Apple Fitness+')])[2]"
+s_apple_news_plus = "(//a[contains(.,'Apple News+')])[1]"
+s_apple_podcasts = "(//a[contains(.,'Apple Podcasts')])[1]"
+s_apple_books = "(//a[contains(.,'Apple Books')])[1]"
+s_app_store = "(//a[contains(.,'App Store')])[1]"
+
+#Support
+s_apple_tv_plus_support = "//a[contains(.,'Apple TV+ Support')]"
+s_apple_music_support = "//a[contains(.,'Apple Music Support')]"
+
+#ACCESSORIES
+#Shop Accessories
+s_shop_all_accessories = "//a[contains(.,'Shop All Accessories')]"
+s_shop_accessories_mac = "(//a[contains(.,'Mac')])[16]"
+s_shop_accessories_ipad = "(//a[contains(.,'iPad')])[16]"
+s_shop_accessories_iphone = "(//a[contains(.,'iPhone')])[14]"
+s_shop_accessories_apple_watch = "(//a[contains(.,'Apple Watch')])[14]"
+s_shop_accessories_air_pods = "(//a[contains(.,'AirPods')])[11]"
+s_shop_accessories_tv_home = "(//a[contains(.,'TV & Home')])[4]"
+
+#Explore Accessories
+s_made_by_apple = "//a[contains(.,'Made by Apple')]"
+s_beats_by_dr_dre = "//a[contains(.,'Beats by Dr. Dre')]"
+s_air_tag = "(//a[contains(.,'AirTag')])[1]"
+
+#SUPPORT
+#Explore support
+s_iphone_explore_support = "(//a[contains(.,'iPhone')])[15]"
+s_mac_explore_support = "(//a[contains(.,'Mac')])[17]"
+s_ipad_explore_support = "(//a[contains(.,'iPad')])[17]"
+s_apple_watch_explore_support = "(//a[contains(.,'Watch')])[17]"
+s_air_pods_explore_support = "(//a[contains(.,'AirPods')])[12]"
+s_music_explore_support = "(//a[contains(.,'Music')])[5]"
+s_tv_home_explore_support = "(//a[contains(.,'TV')])[12]"
+s_explore_support = "//a[contains(.,'Explore Support')]"
+
+#Get Help
+s_community = "//a[contains(.,'Community')]"
+s_check_coverage = "//a[contains(.,'Check Coverage')]"
+s_repair = "//a[contains(.,'Repair')]"
+s_contact_us = "//a[contains(.,'Contact Us')]"
+
+#Helpful topics
+s_get_apple_care_plus = "//a[contains(.,'Get AppleCare+')]"
+s_apple_id_and_password = "//a[contains(.,'Apple ID & Password')]"
+s_billing_and_subscriptions = "//a[contains(.,'Billing & Subscriptions')]"
+s_find_me = "//a[contains(.,'Find My')]"
+s_accessibility = "(//a[contains(.,'Accessibility')])[1]"
+
+
+
---
+++ BrowserStack/my_key.py
@@ -0,0 +1,5 @@
+#"Please past here your acsess key from your BrowserStack account to run the script above"
+
+BROWSERSTACK_USERNAME = "your_BROWSERSTACK_USERNAME"
+BROWSERSTACK_ACCESS_KEY = "your_BROWSERSTACK_ACCESS_KEY"
+
